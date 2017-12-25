0a48426649dbf8a3b2d3ebd2d3ad7ff0f2da730f
MMenge <marja.menge@gmail.com>
2017-12-25 17:48:51 +0100

    update index with mysql connection

--- www/index.php
+++ www/index.php
@@ -1,1 +1,12 @@
-<?php phpinfo();
+<?php
+
+$connectdb = "my_app_db";
+$hostdb = "172.16.238.13";
+$userdb = "my_app";
+$passdb = "root";
+
+mysql_connect($hostdb, $userdb, $passdb) or
+die("Could not connect: " . mysql_error());
+mysql_select_db("$connectdb");
+
+phpinfo();

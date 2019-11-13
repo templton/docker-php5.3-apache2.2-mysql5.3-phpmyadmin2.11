96c68ecda8574a8255db859e69237e29d95cc625
Salamatov <dimasalam@yandex.ru>
2019-11-13 18:37:05 +0300

    исправил docker-compose

--- www/index.php
+++ www/index.php
@@ -1,12 +1,11 @@
 <?php
 
-$connectdb = "my_app_db";
-$hostdb = "172.16.238.13";
-$userdb = "my_app";
+$connectdb = "yii_db";
+$hostdb = "mysql";
+$userdb = "user";
 $passdb = "root";
 
-mysql_connect($hostdb, $userdb, $passdb) or
-die("Could not connect: " . mysql_error());
+mysql_connect($hostdb, $userdb, $passdb) or die("Could not connect: " . mysql_error());
 mysql_select_db("$connectdb");
 
 phpinfo();
